86def911bba3ce20889d7feb3381ed1c572a272b
Hammad-Ur-Rehman <rehmanhammad100@gmail.com>
2026-07-12 00:37:14 +0500

    commit changes

--- Hello/Hello/settings.py
+++ Hello/Hello/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-11^^e%5m#$&mxn(ku0v(vxjqss7uq!o+7aoz!u)nej^zrxrs5d'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
